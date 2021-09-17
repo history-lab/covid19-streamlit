adc4fa2b54e480600cf469075bf0320a9561e932
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-09-17 13:28:37 -0400

    Added form and date selectors

--- c19em_app.py
+++ c19em_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import psycopg2
+import datetime
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 
@@ -55,25 +56,40 @@ c = alt.Chart(cntsdf).mark_bar().encode(
 )
 st.altair_chart(c, use_container_width=True)
 
-""" ## Individual Emails """
-emqry = """
-select sent, subject, topic, from_email "from", to_emails "to", cc_emails cc,
-       body, e.email_id, file_pg_start pg_number
-    from covid19.emails e
-       left join top_topic_emails t on (e.email_id = t.email_id)
-    where sent >= '2020-01-01'
-    order by sent nulls last
-"""
-emdf = pd.read_sql_query(emqry, conn)
-emdf['sent'] = pd.to_datetime(emdf['sent'], utc=True)
-#
-gb = GridOptionsBuilder.from_dataframe(emdf)
-gb.configure_pagination()
-gb.configure_default_column(groupable=True, value=True, enableRowGroup=True,
-                            aggFunc="sum", editable=True)
-gridOptions = gb.build()
+""" ## Search Emails """
+query = False
+form = st.form('query_params')
+# persons = form.multiselect('Person(s):', ['Fauci', 'Collins'])
+# orgs = form.multiselect('Organization(s):', ['NIH', 'White House'])
+# locations = form.multiselect('Location(s):', ['Brooklyn'])
+begin_date = form.date_input('Start Date', datetime.date(2020, 1, 23))
+end_date = form.date_input('End Date', datetime.date(2020, 5, 6))
+query = form.form_submit_button(label='Execute Search')
 
-AgGrid(emdf, gridOptions=gridOptions, enable_enterprise_modules=True)
+if query:
+    """ ## Search Results """
+    # entities = persons + orgs + locations
+    # st.write(entities)
+    selfrom = """
+    select sent, subject, topic, from_email "from", to_emails "to",
+           cc_emails cc, body, e.email_id, file_pg_start pg_number
+        from covid19.emails e left join covid19.top_topic_emails t
+            on (e.email_id = t.email_id)"""
+    where = f"where sent between '{begin_date}' and '{end_date}' "
+    orderby = 'order by sent'
+    st.write(where)
+    emqry = selfrom + where + orderby
+    emdf = pd.read_sql_query(emqry, conn)
+    emdf['sent'] = pd.to_datetime(emdf['sent'], utc=True)
+    #
+    gb = GridOptionsBuilder.from_dataframe(emdf)
+    gb.configure_pagination()
+    gb.configure_default_column(groupable=True, value=True,
+                                enableRowGroup=True, aggFunc="sum",
+                                editable=True)
+    gridOptions = gb.build()
+
+    AgGrid(emdf, gridOptions=gridOptions, enable_enterprise_modules=True)
 
 """
 ## About
