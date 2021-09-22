e43d46a76faf1f6eaa74a7fe37fe0988e04dc7c7
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-09-22 18:34:33 -0400

    Added basic row selection to the search results

--- c19em_app.py
+++ c19em_app.py
@@ -4,8 +4,10 @@ import pandas as pd
 import altair as alt
 import psycopg2
 import datetime
+import base64
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
 
 
 st.set_page_config(page_title="FOIA Explorer: COVID-19 Emails", layout="wide")
@@ -81,50 +83,63 @@ orgs = form.multiselect('Organization(s):', org_list)
 locations = form.multiselect('Location(s):', loc_list)
 query = form.form_submit_button(label='Execute Search')
 
-if query:
-    """ ## Search Results """
-    entities = persons + orgs + locations
-    selfrom = """
-    select sent, subject, topic, from_email "from", to_emails "to",
-           cc_emails cc, body, e.email_id, file_pg_start pg_number
-        from covid19.emails e left join covid19.top_topic_emails t
-            on (e.email_id = t.email_id)"""
-    where = f"where sent between '{begin_date}' and '{end_date}' "
-    where_ent = ''
-    orderby = 'order by sent'
-    qry_explain = where
-    if entities:
-        # build entity in list
-        entincl = '('
-        for e in entities:
-            entincl += f"'{e}', "
-        entincl = entincl[:-2] + ')'
-        # form subquery
-        where_ent = """
-        and e.email_id in
-            (select eem.email_id
-                from covid19.entities ent join covid19.entity_emails eem
-                    on (ent.entity_id = eem.entity_id)
-             where ent.entity in """ + f'{entincl}) '
-        qry_explain += f"and email references at least one of {entincl}"
-    st.write(qry_explain)
-    # execute query
-    emqry = selfrom + where + where_ent + orderby
-    emdf = pd.read_sql_query(emqry, conn)
-    emdf['sent'] = pd.to_datetime(emdf['sent'], utc=True)
-    #
-    gb = GridOptionsBuilder.from_dataframe(emdf)
-    gb.configure_pagination()
-    gb.configure_default_column(groupable=True, value=True,
-                                enableRowGroup=True, aggFunc="sum",
-                                editable=True)
-    gridOptions = gb.build()
+# if query:
+""" ## Search Results """
+entities = persons + orgs + locations
+selfrom = """
+select sent, subject, topic, from_email "from", to_emails "to", cc_emails cc,
+       body, e.email_id, file_pg_start pg_number
+    from covid19.emails e left join covid19.top_topic_emails t
+        on (e.email_id = t.email_id)"""
+where = f"where sent between '{begin_date}' and '{end_date}' "
+where_ent = ''
+orderby = 'order by sent'
+qry_explain = where
+if entities:
+    # build entity in list
+    entincl = '('
+    for e in entities:
+        entincl += f"'{e}', "
+    entincl = entincl[:-2] + ')'
+    # form subquery
+    where_ent = """
+    and e.email_id in
+        (select eem.email_id
+            from covid19.entities ent join covid19.entity_emails eem
+                on (ent.entity_id = eem.entity_id)
+            where ent.entity in """ + f'{entincl}) '
+    qry_explain += f"and email references at least one of {entincl}"
+st.write(qry_explain)
+# execute query
+emqry = selfrom + where + where_ent + orderby
+emdf = pd.read_sql_query(emqry, conn)
+emdf['sent'] = pd.to_datetime(emdf['sent'], utc=True)
+# download results as CSV
+csv = emdf.to_csv().encode('utf-8')
+st.download_button(label="CSV download", data=csv,
+                   file_name='foia-covid19.csv', mime='text/csv')
+# generate AgGrid
+gb = GridOptionsBuilder.from_dataframe(emdf)
+gb.configure_pagination()
+gb.configure_selection('single')
+gb.configure_default_column(groupable=True, value=True,
+                            enableRowGroup=True, aggFunc="sum",
+                            editable=True)
+gridOptions = gb.build()
 
-    AgGrid(emdf, gridOptions=gridOptions, enable_enterprise_modules=True)
-    # download results as CSV
-    csv = emdf.to_csv().encode('utf-8')
-    st.download_button(label="CSV download", data=csv,
-                       file_name='foia-covid19.csv', mime='text/csv')
+grid_response = AgGrid(emdf, gridOptions=gridOptions,
+                       reload_data=True,
+                       enable_enterprise_modules=True,
+                       allow_unsafe_jscode=True,
+                       update_mode=GridUpdateMode.SELECTION_CHANGED)
+selected = grid_response['selected_rows']
+st.write(selected)
+"""## Document Preview"""
+with open('./pdfs/fauci-cnn.pdf', "rb") as f:
+    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" \
+width="100%" height="1100" type="application/pdf">'
+st.markdown(pdf_display, unsafe_allow_html=True)
 """
 ## About
 The FOIA Explorer and associated tools were created by Columbia
