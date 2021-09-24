967c0e0c2e389853385b573bfe97ac02771e8fa8
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-09-24 18:11:28 -0400

    Changed PDF viewer from embed to iframe

--- c19em_app.py
+++ c19em_app.py
@@ -149,7 +149,7 @@ leopold-nih-foia-anthony-fauci-emails.pdf#page={pg}'
     response = requests.get(preview_url)
     if response.content:
         base64_pdf = base64.b64encode(response.content).decode('utf-8')
-        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" \
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" \
 width="100%" height="1100" type="application/pdf">'
         st.markdown(pdf_display, unsafe_allow_html=True)
     else:
